3d1ad755abc2a8000f7b655f9ab80c20f50bdf9d
Kirill Grachev <kirill.grachoff@gmail.com>
2024-01-15 02:26:26 +0300

    Solve Efficient Broadcast, Part I https://fly.io/dist-sys/3d/
    Results:
    --------
     :availability {:valid? true, :ok-fraction 1.0},
     :net {:all {:send-count 45112,
                 :recv-count 45112,
                 :msg-count 45112,
                 :msgs-per-op 26.567726},
           :clients {:send-count 3496, :recv-count 3496, :msg-count 3496},
           :servers {:send-count 41616,
                     :recv-count 41616,
                     :msg-count 41616,
                     :msgs-per-op 24.508835},
           :valid? true},
    --------
                :duplicated-count 0,
                :valid? true,
                :lost-count 0,
                :lost (),
                :stable-count 867,
                :stale-count 2,
                :stale (714 804),
                :never-read-count 0,
                :stable-latencies {0 0, 0.5 0, 0.95 0, 0.99 0, 1 32},
                :attempt-count 867,
                :never-read (),
                :duplicated {}

--- src/bin/broadcast.rs
+++ src/bin/broadcast.rs
@@ -1,6 +1,6 @@
 /// ```bash
 /// $ cargo build
-/// $ maelstrom test -w broadcast --bin ./target/debug/broadcast --node-count 5 --time-limit 20 --rate 10 --nemesis partition
+/// $ maelstrom test -w broadcast --bin ./target/debug/broadcast --node-count 25 --time-limit 20 --rate 100 --latency 100
 /// ````
 use async_trait::async_trait;
 use maelstrom::protocol::Message;
